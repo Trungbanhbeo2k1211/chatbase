ab275d5158f2b314b98ba393f6508f47a49f62af
Trungbanhbeo2k1211 <166927277+Trungbanhbeo2k1211@users.noreply.github.com>
2025-07-17 23:25:25 +0700

    Update run.py

--- run.py
+++ run.py
@@ -71,4 +71,5 @@ def home():
                            online_ids=list(online_users))
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port, debug=True)
