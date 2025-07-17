2aa67c4b01d35af6a92d5d2871f9db772555d3f7
Trungbanhbeo2k1211 <166927277+Trungbanhbeo2k1211@users.noreply.github.com>
2025-07-17 22:48:42 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:123456@localhost:5432/chatdb"
+    SQLALCHEMY_DATABASE_URI = "postgresql://trung:coD8KSdcUpFf9dQvHBGhqd52KZazoZRW@dpg-d1shkvbipnbc73e3uhsg-a.oregon-postgres.render.com/chatdb_s5yo"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Thêm cấu hình cho session
